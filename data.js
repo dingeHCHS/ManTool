c8055dfaffa8e1ba0d76409e4c477e30e141cae6
dingeHCHS <69469881+dingeHCHS@users.noreply.github.com>
2021-02-22 11:31:08 -0600

    sum

---
+++ data.js
@@ -0,0 +1,85 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-machinetool",
+      "name": "MachineTool",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 0.05878595828220057,
+        "pitch": 0.16391401746609624,
+        "fov": 1.2933824216075565
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 0,
+          "pitch": 0,
+          "title": "MachineTool Intro",
+          "text": "Text"
+        },
+        {
+          "yaw": 0.6345380752417853,
+          "pitch": -0.062168513909771406,
+          "title": "MT1",
+          "text": "Text"
+        },
+        {
+          "yaw": 1.2876707191205998,
+          "pitch": -0.009031420504069132,
+          "title": "MT2",
+          "text": "Text"
+        },
+        {
+          "yaw": 1.8534271982911372,
+          "pitch": -0.010269850922618673,
+          "title": "MT3",
+          "text": "Text"
+        },
+        {
+          "yaw": 2.6062391694656473,
+          "pitch": -0.024160271270014633,
+          "title": "MT4",
+          "text": "Text"
+        },
+        {
+          "yaw": 3.1392364722845123,
+          "pitch": -0.03265586098711992,
+          "title": "MT5",
+          "text": "Text"
+        },
+        {
+          "yaw": -2.6581331733091975,
+          "pitch": -0.0348158387720332,
+          "title": "MT6",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
