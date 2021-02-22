89925296b0c709fa179ab07e5995fdaf8067b8dd
dingeHCHS <69469881+dingeHCHS@users.noreply.github.com>
2021-02-22 11:33:05 -0600

    Update data.js

--- data.js
+++ data.js
@@ -34,43 +34,43 @@ var APP_DATA = {
           "yaw": 0,
           "pitch": 0,
           "title": "MachineTool Intro",
-          "text": "Text"
+          "text": '<iframe src="https://drive.google.com/file/d/1KO4wDPDPi1fvwgtOSRpziE17ZXFICHc9/preview" width="640" height="480"></iframe>'
         },
         {
           "yaw": 0.6345380752417853,
           "pitch": -0.062168513909771406,
           "title": "MT1",
-          "text": "Text"
+          "text": '<iframe src="https://drive.google.com/file/d/1KG5WLK8pZk3h9le1D1uwTo7hma9yvjsU/preview" width="640" height="480"></iframe>'
         },
         {
           "yaw": 1.2876707191205998,
           "pitch": -0.009031420504069132,
           "title": "MT2",
-          "text": "Text"
+          "text": '<iframe src="https://drive.google.com/file/d/1KP9ogm5Xb_sGBtfDrS4spQQ8L-1OP8zI/preview" width="640" height="480"></iframe>'
         },
         {
           "yaw": 1.8534271982911372,
           "pitch": -0.010269850922618673,
           "title": "MT3",
-          "text": "Text"
+          "text": '<iframe src="https://drive.google.com/file/d/1KbPJ-MsEpBd1Etg7n7OTEZcZNMx4CzQx/preview" width="640" height="480"></iframe>'
         },
         {
           "yaw": 2.6062391694656473,
           "pitch": -0.024160271270014633,
           "title": "MT4",
-          "text": "Text"
+          "text": '<iframe src="https://drive.google.com/file/d/1KcfqTLiJpo6MTnZOR4S7mhI02YyKn1lI/preview" width="640" height="480"></iframe>'
         },
         {
           "yaw": 3.1392364722845123,
           "pitch": -0.03265586098711992,
           "title": "MT5",
-          "text": "Text"
+          "text": '<iframe src="https://drive.google.com/file/d/1Kd0ENK742vuG-g15aYzMTbUzs2fYfkbK/preview" width="640" height="480"></iframe>'
         },
         {
           "yaw": -2.6581331733091975,
           "pitch": -0.0348158387720332,
           "title": "MT6",
-          "text": "Text"
+          "text": '<iframe src="https://drive.google.com/file/d/1KeQJldXn7XGyCJnGk6NPbBDz7k0rCOaB/preview" width="640" height="480"></iframe>'
         }
       ]
     }
